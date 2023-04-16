e0ec6455432c605cdc6a105f7a24a3e915daf118
SamuelWade <samuelrcwade@gmail.com>
2023-04-16 10:19:31 -0400

    lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+function EyesOnMe(){
+    function focusEvent(){
+        console.log('Good!')
+    }
+    function blurEvent(){
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onFocus={focusEvent} onBlur={blurEvent}>Eyes on me</button>
+        //how do I use the same button to listen to 2 seperate events?
+    )
+}
+
+export default EyesOnMe;
